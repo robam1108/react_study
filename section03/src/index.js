2ddc22e8b14b6d774a7505e004ff090503189fb6
boram <108602668+robam1108@users.noreply.github.com>
2025-02-12 15:01:58 +0900

    node.js 기초

---
+++ section03/src/index.js
@@ -0,0 +1,11 @@
+// import mul from "./math.js";
+// const {add,sub} = require("./math");
+import mul, {add, sub} from "./math.js";
+import randomColor from "randomcolor"; // 라이브러리는 경로 표시 X
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2,3));
