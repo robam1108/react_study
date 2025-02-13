a3098bf8c6b8ec7ed1d3b651a0f34e2bd9946e67
boram <108602668+robam1108@users.noreply.github.com>
2025-02-13 14:10:43 +0900

    react 기초

---
+++ section05/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/footer'
+import Button from "./components/Button"
+
+function App() {
+
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
+  return (
+    <>
+        <Button {...buttonProps}/>
+        <Button text={"카페"}/>
+        <Button text={"블로그"}>
+          <div>자식요소</div>
+          <Header/>
+        </Button>
+    </>
+  )
+}
+
+export default App
